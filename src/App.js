8f73de619702a4d7bcf5a742bf6aa8f2020ac890
henrikpoa <henrik_poa@hotmail.com>
2023-04-04 10:34:00 -0300

    add POST

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ const url = 'http://localhost:8080/users';
 function App() {
   const [users, setUsers] = useState([]);
 
+  const[name, setName] = useState("");
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[phone, setPhone] = useState("");
+
   // 1 - resgatando dados  
   useEffect(() =>{
     async function fetchData() {
@@ -20,13 +25,60 @@ function App() {
     fetchData();
   }, []);
   
+  // add users
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      name, email, password, phone,
+    };
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user),
+    });
+
+    const addedUser = await res.json();
+
+    setUsers((prevUsers) => [...prevUsers, addedUser]);
+
+    setName('');
+    setEmail('');
+    setPassword('');
+    setPhone('');
+  };
+
   return (
     <div className="App">
+      <div className='add-user'><h1>Criar Usuários: </h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={(e)=> setName(e.target.value)}/>
+          </label>
+          <label>
+            Email:
+            <input type="email" value={email} name='email' onChange={(e)=> setEmail(e.target.value)}/>
+          </label>
+          <label>
+            Password:
+            <input type="current-password" value={password} name='password' onChange={(e)=> setPassword(e.target.value)}/>
+          </label>
+          <label>
+            Telefone:
+            <input type="number" value={phone} name='phone' onChange={(e)=> setPhone(e.target.value)}/>
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
       <h1>Lista de Usuários</h1>
       <ul>
       {users.map((user) => (
         <li key={user.id}>
-          {user.id} - {user.name}
+          {user.id} - {user.name} - {user.email} - {user.phone}
         </li>
       ))}
       </ul>
